0b4f8bd7626e699f42c5f65ad5773268de062001
Chidera <chideraonyekachi04@gmail.com>
2022-07-01 14:19:17 +0100

    sail Array project

---
+++ main.js
@@ -0,0 +1,95 @@
+ let no = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+ let firstName = ["Thompson", "Johnson", "Chisom", "Jide", "Williams", "Nancy", "Nicholas", "Ahmed", "Halima", "Richard"];
+ let lastName = ["Anthony", "Mary", "Onyeka", "Olabisi", "Ajayi", "Chukwuemeka", "Uchemba", "Olayinka", "Mohammed", "Adeyemi"];
+ let age = [32, 23, 28, 29, 30, 27, 26, 24, 26, 41];
+ let sex = ["Male", "Female", "Female", "Male", "Male", "Female", "Male", "Male", "Female", "Male"];
+ let position = ["Head of Finance Deparment", "Software Engineer", "Human Resources Manager", "Assisant Head (Software Department)", "Head of Software Department", "Secretary", "Software Engineer", "Graphic Designer", "Senior Finance Manager", "C.E.O"]
+ let dateOfEmployment = [" 17/07/2014", "05/03/2019", "21/06/2016", "05/06/2016", "06/09/2015", "23/08/2016", "22/02/2020", "15/08/2020", "30/05/2018", "17/11/2010"]
+
+ let y = document.getElementsByTagName("td");
+
+ y[0].innerHTML = no[0];
+ y[1].innerHTML = firstName[0];
+ y[2].innerHTML = lastName[0];
+ y[3].innerHTML = age[0];
+ y[4].innerHTML = sex[0];
+ y[5].innerHTML = position[0];
+ y[6].innerHTML = dateOfEmployment[0];
+
+ y[7].innerHTML= no[1];
+ y[8].innerHTML = firstName[1];
+ y[9].innerHTML = lastName[1];
+ y[10].innerHTML = age[1];
+ y[11].innerHTML = sex[1];
+ y[12].innerHTML = position[1];
+ y[13].innerHTML = dateOfEmployment[1];
+
+
+ y[14].innerHTML = no[2];
+ y[15].innerHTML = firstName[2];
+ y[16].innerHTML = lastName[2];
+ y[17].innerHTML = age[2];
+ y[18].innerHTML = sex[2];
+ y[19].innerHTML = position[2];
+ y[20].innerHTML = dateOfEmployment[2];
+
+ 
+ y[21].innerHTML = no[3];
+ y[22].innerHTML = firstName[3];
+ y[23].innerHTML = lastName[3];
+ y[24].innerHTML = age[3];
+ y[25].innerHTML = sex[3];
+ y[26].innerHTML = position[3];
+ y[27].innerHTML = dateOfEmployment[3];
+
+
+ y[28].innerHTML = no[4];
+ y[29].innerHTML = firstName[4];
+ y[30].innerHTML = lastName[4];
+ y[31].innerHTML = age[4];
+ y[32].innerHTML = sex[4];
+ y[33].innerHTML = position[4];
+ y[34].innerHTML = dateOfEmployment[4];
+
+
+ y[35].innerHTML = no[5];
+ y[36].innerHTML = firstName[5];
+ y[37].innerHTML = lastName[5];
+ y[38].innerHTML = age[5];
+ y[39].innerHTML = sex[5];
+ y[40].innerHTML = position[5];
+ y[41].innerHTML = dateOfEmployment[5];
+
+
+ y[42].innerHTML = no[6];
+ y[43].innerHTML = firstName[6];
+ y[44].innerHTML = lastName[6];
+ y[45].innerHTML = age[6];
+ y[46].innerHTML = sex[6];
+ y[47].innerHTML = position[6];
+ y[48].innerHTML = dateOfEmployment[6];
+
+ y[49].innerHTML = no[7];
+ y[50].innerHTML = firstName[7];
+ y[51].innerHTML = lastName[7];
+ y[52].innerHTML = age[7];
+ y[53].innerHTML = sex[7];
+ y[54].innerHTML = position[7];
+ y[55].innerHTML = dateOfEmployment[7];
+
+
+ y[56].innerHTML = no[8];
+ y[57].innerHTML = firstName[8]; 
+ y[58].innerHTML = lastName[8];
+ y[59].innerHTML = age[8];
+ y[60].innerHTML = sex[8];
+ y[61].innerHTML = position[8];
+ y[62].innerHTML = dateOfEmployment[8];
+
+y[63].innerHTML = no[9];
+y[64].innerHTML = firstName[9];
+y[65].innerHTML = lastName[9];
+y[66].innerHTML = age[9];
+y[67].innerHTML = sex[9];
+y[68].innerHTML = position[9];
+y[69].innerHTML = dateOfEmployment[9];
